14e2f10a1a363ed3142a63e7c97f0c0bc84f6225
Phantom <eli12122@skole.telemarkfylke.no>
2025-02-04 11:49:57 +0100

    1

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-window.onload = oppstart;
-
-function oppstart(){
+window.onload = function() {
     document.querySelector("#fyllBensin").onclick = fyllBensin;
-    document.querySelector("#nullstill").onclick = nullstill;
-}
+    document.querySelector("#nullstill").onclick = function() {
+        document.querySelector("#bensinMengde").value = "";
+        document.querySelector("#utskrift").innerHTML = "";
+    };
+};
 
-function fyllBensin(){
-    let bensinMengde = document.querySelector("#bensinMengde").value;
+function fyllBensin() {
+    let bensinMengde = parseFloat(document.querySelector("#bensinMengde").value);
     let pris = 20;
+    let utskrift = "";
 
-    if(bensinMengde > 80){
-        document.querySelector("#utskrift").innerHTML = "Tanken din rommer ikke så mye";
-    } else if(bensinMengde > 0 && bensinMengde < 5){
-        document.querySelector("#utskrift").innerHTML = "Vi selger ikke så små lite kvanta";
-    } else if(bensinMengde <= 0){ 
-        document.querySelector("#utskrift").innerHTML = "Ugyldig mengde";
+    if (bensinMengde > 80) {
+        utskrift = "Tanken din rommer ikke så mye";
+    } else if (bensinMengde > 0 && bensinMengde < 5) {
+        utskrift = "Vi selger ikke så små kvanta";
+    } else if (bensinMengde <= 0) {
+        utskrift = "Ugyldig mengde";
     } else {
         let totalPris = bensinMengde * pris;
-        document.querySelector("#utskrift").innerHTML = `Du må betale ${totalPris.toFixed(2)} kr for ${bensinMengde} liter bensin.`;
+        utskrift = `Du må betale ${totalPris.toFixed(2)} kr for ${bensinMengde} liter bensin.`;
     }
-}
 
-function nullstill(){
-    document.querySelector("#bensinMengde").value = "";
-    document.querySelector("#utskrift").innerHTML = "";
+    document.querySelector("#utskrift").innerHTML = utskrift;
 }
