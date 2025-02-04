d0d322d9dcfdd35218be2b03c36b6dbd26bbc0e9
Phantom <eli12122@skole.telemarkfylke.no>
2025-02-04 11:39:51 +0100

    1

--- script.js
+++ script.js
@@ -6,39 +6,39 @@ function oppstart(){
 }
 
 function fyllBensin(){
-    let liter = document.querySelector("#liter").value;
+    let bensinMengde = document.querySelector("#bensinMengde").value;
     let pris = 21.35;
 
-    if(liter > 80){
+    if(bensinMengde > 80){
         document.querySelector("#utskrift").innerHTML = "Tanken din rommer ikke så mye";
     }
 
-    else if((liter > 0) & (liter < 5)){
+    else if((bensinMengde > 0) & (bensinMengde < 5)){
         document.querySelector("#utskrift").innerHTML = "Vi selger ikke så små kvanta";
     }
 
-    else if(liter<=0){ 
+    else if(bensinMengde<=0){ 
         document.querySelector("#utskrift").innerHTML = "Skriv inn et positivt tall større enn 5"
     }
 
-    else if(liter > 50){
-        let betaling = liter * pris * 0.9;
-        let rabatt = liter * pris *0.1;
+    else if(bensinMengde > 50){
+        let betaling = bensinMengde * pris * 0.9;
+        let rabatt = bensinMengde * pris *0.1;
         let mva_faktor = 0.23;
         let mva = (betaling/(1+mva_faktor)).toFixed(2);
-        document.querySelector("#utskrift").innerHTML = "Du har fyllt " + liter + " liter bensin til "+ pris + " kr literen.<br>Du må betale kr  " + betaling +". Rabatten din er på kr " +rabatt + ".<br> Mva utgjør kr " + mva + " av summen du betaler.";
+        document.querySelector("#utskrift").innerHTML = "Du har fyllt " + bensinMengde + " bensinMengde bensin til "+ pris + " kr bensinMengde.<br>Du må betale kr  " + betaling +". Rabatten din er på kr " +rabatt + ".<br> Mva utgjør kr " + mva + " av summen du betaler.";
 
     }
 
     else{
-        let betaling = liter * pris;
+        let betaling = bensinMengde * pris;
         let mva_faktor = 0.23;
         let mva = (betaling/(1+mva_faktor)).toFixed(2); 
-        document.querySelector("#utskrift").innerHTML = "Du har fyllt " + liter + " liter bensin til "+ pris + " kr literen. <br>Du må betale kr " + betaling + ".<br> Mva utgjør kr " + mva + " av summen du betaler.";
+        document.querySelector("#utskrift").innerHTML = "Du har fyllt " + bensinMengde + " bensinMengde til "+ pris + " kr bensinMengde. <br>Du må betale kr " + betaling + ".<br> Mva utgjør kr " + mva + " av summen du betaler.";
     }
 }
 
 function nullstill(){
     document.querySelector("#utskrift").innerHTML ="";
-    document.querySelector("#liter").value ="";
+    document.querySelector("#bensinMengde").value ="";
 }
