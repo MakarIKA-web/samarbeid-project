4cacfadd3959278719480e47a3b159d07f840d15
Phantom <eli12122@skole.telemarkfylke.no>
2025-02-04 11:41:57 +0100

    1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = oppstart;
 
 function oppstart(){
-    document.querySelector("#fyllBensinKnapp").onclick = fyllBensin;
-    document.querySelector("#nullstillKnapp").onclick = nullstill;
+    document.querySelector("#fyllBensin").onclick = fyllBensin;
+    document.querySelector("#nullstill").onclick = nullstill;
 }
 
 function fyllBensin(){
@@ -11,34 +11,17 @@ function fyllBensin(){
 
     if(bensinMengde > 80){
         document.querySelector("#utskrift").innerHTML = "Tanken din rommer ikke så mye";
-    }
-
-    else if((bensinMengde > 0) & (bensinMengde < 5)){
+    } else if(bensinMengde > 0 && bensinMengde < 5){
         document.querySelector("#utskrift").innerHTML = "Vi selger ikke så små kvanta";
-    }
-
-    else if(bensinMengde<=0){ 
-        document.querySelector("#utskrift").innerHTML = "Skriv inn et positivt tall større enn 5"
-    }
-
-    else if(bensinMengde > 50){
-        let betaling = bensinMengde * pris * 0.9;
-        let rabatt = bensinMengde * pris *0.1;
-        let mva_faktor = 0.23;
-        let mva = (betaling/(1+mva_faktor)).toFixed(2);
-        document.querySelector("#utskrift").innerHTML = "Du har fyllt " + bensinMengde + " bensinMengde bensin til "+ pris + " kr bensinMengde.<br>Du må betale kr  " + betaling +". Rabatten din er på kr " +rabatt + ".<br> Mva utgjør kr " + mva + " av summen du betaler.";
-
-    }
-
-    else{
-        let betaling = bensinMengde * pris;
-        let mva_faktor = 0.23;
-        let mva = (betaling/(1+mva_faktor)).toFixed(2); 
-        document.querySelector("#utskrift").innerHTML = "Du har fyllt " + bensinMengde + " bensinMengde til "+ pris + " kr bensinMengde. <br>Du må betale kr " + betaling + ".<br> Mva utgjør kr " + mva + " av summen du betaler.";
+    } else if(bensinMengde <= 0){ 
+        document.querySelector("#utskrift").innerHTML = "Ugyldig mengde";
+    } else {
+        let totalPris = bensinMengde * pris;
+        document.querySelector("#utskrift").innerHTML = `Du må betale ${totalPris.toFixed(2)} kr for ${bensinMengde} liter bensin.`;
     }
 }
 
 function nullstill(){
-    document.querySelector("#utskrift").innerHTML ="";
-    document.querySelector("#bensinMengde").value ="";
+    document.querySelector("#bensinMengde").value = "";
+    document.querySelector("#utskrift").innerHTML = "";
 }
