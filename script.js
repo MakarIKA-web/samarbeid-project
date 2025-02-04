087c67b3656bc1dae47927abfa1d0e524e433de7
Phantom <eli12122@skole.telemarkfylke.no>
2025-02-04 08:43:14 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculateTotalPrice(liters) {
+    const pricePerLiter = 20; // Pris per liter i kroner
+    const vatRate = 0.25; // MVA rate
+
+    if (liters < 0) {
+        return 'Vi tar ikke bensin i retur.';
+    }
+
